fdff3e8ab5e9f50caa158ff06c97785c4f50ae6d
Christopher Bebry <invalidsyntax@gmail.com>
2013-02-24 03:39:15 +0000

    Added require for a local file example.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var ourDB = require('./config/development');
 
 // Create a server with a host and port
 var server = new Hapi.Server('0.0.0.0', 8000);
@@ -10,13 +11,15 @@ var hello = {
         request.reply({ greeting: 'hello world' });
     }
 };
-
+var test = {
+    handler: function (request) {
+        var dbname = ourDB.config.db;
+        request.reply({result: dbname});
+    }
+};
 // Add the route
-server.route({
-    method: 'GET',
-    path: '/hello',
-    config: hello
-});
+server.route({ method: 'GET', path: '/hello', config: hello });
+server.route({ method: 'GET', path: '/test', config: test });
 
 // Start the server
 server.start();
