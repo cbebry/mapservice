3355a40a8e65adcafee3fd295714017a166ec227
Christopher Bebry <invalidsyntax@gmail.com>
2013-02-26 20:47:32 +0000

    Added an empty example test, and made it so npm test runs the test indicated in the makefile.

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+
+describe('Tile', function() {
+    describe('.add()', function() {
+        it('should create a Tile object with the required children objects.', function() {
+            //TODO:
+            // require Tile model file
+            // create the Tile object
+            // assert() some things about the instantiation
+        });
+    });
+});
