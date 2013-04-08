36cabb8b7ede6d4509fead1ecba4208b4b722371
Christopher Bebry <invalidsyntax@gmail.com>
2013-04-08 02:47:37 +0000

    Travis support.

--- test/mesh_test.js
+++ test/mesh_test.js
@@ -1,59 +1,42 @@
-var assert = require("assert");
+var assert = require('assert');
+var async = require('async');
 //var Sequelize = require("sequelize");
 
 describe('Mesh', function() {
     describe('{associations}', function() {
         it('should belong to a Tile, and have a one-to-many relationship with both MeshVertex and MeshVertexIndex.', function(done) {
-            //var devconfig = require('../config/database').config;
-
-            //var dbname = devconfig.db;
-            //var dbhostname = devconfig.hostname;
-            //var dbport = devconfig.port;
-            //var dbuser = devconfig.user;
-            //var dbpassword = devconfig.password;
-                        
-            
-            //var sequelize = new Sequelize(dbname, dbuser, dbpassword, {
-            //    host: dbhostname,
-            //    port: dbport
-            //});
-            
             // require Associated model files
             var models = require(__dirname + "/../app/models/");
             var Mesh = models.Mesh;
             var MeshVertex = models.MeshVertex;
             var MeshVertexIndex = models.MeshVertexIndex;
-            //var Mesh = sequelize.import(__dirname + "/../app/models/mesh.model");
-            //var MeshVertex = sequelize.import(__dirname + "/../app/models/mesh.vertex.model");
-            //var MeshVertexIndex = sequelize.import(__dirname + "/../app/models/mesh.vertex_index.model");
             
             // create instance of the object
             // assert() some things about the instantiation's associations
-            console.log("test");
-            //assert(false);
-            Mesh.create({ /* */ }).success(function(mesh) {
-                MeshVertex.create({ /* */ }).success(function(meshVertex) {
-                    mesh.hasMeshVertex(meshVertex).success(function(result) {
-                        // result would be false
-                        console.log("result1:" + result);
-                        assert(result == false);
-                        mesh.addMeshVertex(meshVertex).success(function() {
-                            mesh.hasMeshVertex(meshVertex).success(function(result) {
-                                // result would be true
-                                console.log("result2:" + result);
-                                assert(result == true);
-                                done();
-                            })
-                        })
-                    })
-                })
-            })
-
-            //TODO:
+            var testRelateMeshWithMeshVertex = function () {
+                Mesh.create({ /* */ }).success(function(mesh) {
+                    MeshVertex.create({ /* */ }).success(function(meshVertex) {
+                        mesh.hasMeshVertex(meshVertex).success(function(result) {
+                            // result would be false
+                            console.log("result1:" + result);
+                            assert(result == false);
+                            mesh.addMeshVertex(meshVertex).success(function() {
+                                mesh.hasMeshVertex(meshVertex).success(function(result) {
+                                    // result would be true
+                                    console.log("result2:" + result);
+                                    assert(result == true);
+                                    done();
+                                });
+                            });
+                        });
+                    });
+                });
+            }
             
             
-            
-            //(false);
+            async.parallel({
+                relate_mesh_to_meshVertex: testRelateMeshWithMeshVertex
+            }, callback);
         });
     });
 });
