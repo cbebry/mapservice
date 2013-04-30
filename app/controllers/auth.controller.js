9f94d3c2cb95e37d40d169148b14fd30ff40bf77
Christopher Bebry <invalidsyntax@gmail.com>
2013-04-30 01:29:58 +0000

    Basic auth implemented.

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,31 @@
+var Bcrypt = require('bcrypt');
+
+exports.getBasic = function() {
+    return {
+		auth: { strategies: ['basic'] },
+        handler: function(request) {
+            request.reply('Success');
+        }
+    };
+};
+exports.getValidate = function() {
+	return internals.validate;
+};
+
+var internals = {};
+
+internals.validate = function (username, password, callback) {
+	Bcrypt.compare(password, internals.passwords[username], function (err, isValid) {
+		callback(null, isValid , internals.users[username]);
+	});
+};
+
+internals.passwords = {
+    john: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm'            // password: secret
+};
+
+internals.users = {
+    john: {
+        user: 'john'
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+var AuthController = require('./app/controllers/auth.controller');
 var Hapi = require('hapi'),
-	options = {	},
+
+	options = {
+		auth: {
+			'basic': {
+				scheme: 'basic',
+				validateFunc: AuthController.getValidate()
+			}
+		}
+	},
     serverConfig = require('./config/config').config,
     server = new Hapi.Server(serverConfig.hostname, serverConfig.port, options);
 
@@ -11,36 +20,14 @@ server.views({
     path: __dirname + '/app/views'
 });
 
+
+
 /*
-
-var Bcrypt = require('bcrypt');
-var users = {
-    john: {
-        username: 'john',
-        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
-        name: 'John Doe',
-        id: '2133d32a'
-    }
-};
-
-var zvalidate = function (username, password, callback) {
-
-    var user = users[username];
-    if (!user) {
-        return callback(null, false);
-    }
-
-    Bcrypt.compare(password, user.password, function (err, isValid) {
-
-        callback(err, isValid, { id: user.id, name: user.name });
-    });
-};
-*/
-    //server.auth('simple', {
-    //    scheme: 'basic', 
-    //    validateFunc: zvalidate
-    //});
-    
+server.auth('simple', {
+    scheme: 'basic', 
+    validateFunc: zvalidate
+});
+    */
 
 // Define the routes
 var test = {
@@ -51,19 +38,21 @@ var test = {
 
 var TileController = require('./app/controllers/tile.controller');
 var HomeController = require('./app/controllers/home.controller');
-var AuthController = require('./app/controllers/auth.controller');
 //var UtilController = require('./app/controllers/util.controller');
 
 
 // Add the routes
 
-server.route({ method: 'GET', path: '/{path*}', handler: { directory: { path: './public/', listing: true } } });
+//server.route({ method: 'GET', path: '/auth', config: AuthController.auth() });
 
-server.route({ method: 'GET', path: '/test', config: test });
-server.route({ method: 'GET', path: '/', config: HomeController.view() });
-server.route({ method: 'POST', path: '/tiles/add', config: TileController.add() });
-server.route({ method: 'GET', path: '/tiles/up', config: TileController.up("testparam") });
-//server.route({ method: 'GET', path: '/auth', config: { auth: 'simple' } });
+server.route([
+	{ method: 'GET', path: '/{path*}', handler: { directory: { path: './public/', listing: true } } },
+	{ method: 'GET', path: '/test', config: test },
+	{ method: 'GET', path: '/', config: HomeController.view() },
+	{ method: 'POST', path: '/tiles/add', config: TileController.add() },
+	{ method: 'GET', path: '/tiles/up', config: TileController.up("testparam") },
+	{ method: 'GET', path: '/basic', config: AuthController.getBasic() }
+]);
 
 // Start the server
 server.start();
